692241ad4fc249620a9f25f229c5cf3cbcb23e4d
Victor Li <victor.li158@gmail.com>
2018-03-12 16:18:09 -0700

    Add setupReplay function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 // refactor click listeners
 // refactor checkVictory
 // make the cell divs clickable and use nearest or something to change the image
+// add a play again button
 
 $(document).ready(function() {
   let currentPlayer = 'x';
@@ -15,225 +16,259 @@ $(document).ready(function() {
   let nine = $('#nine');
   let places = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
   let player = $('#player');
+  let replay = $('#replay');
 
   function removeListeners() {
     $('#one, #two, #three, #four, #five, #six, #seven, #eight, #nine').off();
+  }
+
+  function setupReplay() {
+    replay.css('display', 'block');
+    replay.on('click', 'button', function() {
+      places = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+      currentPlayer = 'x';
+      $('img').attr('src', 'images/blank.png');
+      player.attr('src', 'images/x.png');
+      addListeners();
+      replay.css('display', 'none');
+    });
   }
 
   function checkVictory() {
     if (places[0][0] === 'x' && places[0][1] === 'x' && places[0][2] === 'x'){
       alert('X wins!');
       removeListeners();
+      setupReplay();
     } else if (places[0][0] === 'x' && places[1][0] === 'x' && places[2][0] === 'x'){
       alert('X wins!');
       removeListeners();
+      setupReplay();
     } else if (places[0][1] === 'x' && places[1][1] === 'x' && places[2][1] === 'x') {
       alert('X wins!');
       removeListeners();
+      setupReplay();
     } else if (places[1][0] === 'x' && places[1][1] === 'x' && places[1][2] === 'x') {
       alert('X wins!');
       removeListeners();
+      setupReplay();
     } else if (places[2][0] === 'x' && places[2][1] === 'x' && places[2][2] === 'x') {
       alert('X wins!');
       removeListeners();
+      setupReplay();
     } else if (places[0][2] === 'x' && places[1][2] === 'x' && places[2][2] === 'x') {
       alert('X wins!');
       removeListeners();
+      setupReplay();
     } else if (places[0][2] === 'x' && places[1][1] === 'x' && places[2][0] === 'x') {
       alert('X wins!');
       removeListeners();
+      setupReplay();
     } else if (places[0][0] === 'x' && places[1][1] === 'x' && places[2][2] === 'x') {
       alert('X wins!');
       removeListeners();
+      setupReplay();
     } else if (places[0][0] === 'o' && places[0][1] === 'o' && places[0][2] === 'o'){
       alert('O wins!');
       removeListeners();
+      setupReplay();
     } else if (places[0][0] === 'o' && places[1][0] === 'o' && places[2][0] === 'o'){
       alert('O wins!');
       removeListeners();
+      setupReplay();
     } else if (places[0][1] === 'o' && places[1][1] === 'o' && places[2][1] === 'o') {
       alert('O wins!');
       removeListeners();
+      setupReplay();
     } else if (places[1][0] === 'o' && places[1][1] === 'o' && places[1][2] === 'o') {
       alert('O wins!');
       removeListeners();
+      setupReplay();
     } else if (places[2][0] === 'o' && places[2][1] === 'o' && places[2][2] === 'o') {
       alert('O wins!');
       removeListeners();
+      setupReplay();
     } else if (places[0][2] === 'o' && places[1][2] === 'o' && places[2][2] === 'o') {
       alert('O wins!');
       removeListeners();
+      setupReplay();
     } else if (places[0][2] === 'o' && places[1][1] === 'o' && places[2][0] === 'o') {
       alert('O wins!');
       removeListeners();
+      setupReplay();
     } else if (places[0][0] === 'o' && places[1][1] === 'o' && places[2][2] === 'o') {
       alert('O wins!');
       removeListeners();
+      setupReplay();
     } else if (!places[0].includes(0) && !places[1].includes(0) && !places[2].includes(0)) {
       alert('Tie!');
       removeListeners();
+      setupReplay();
     }
   }
 
-  one.on('click', function() {
-    if (places[0][0] === 0 && currentPlayer === 'x') {
-      $(this).attr('src', 'images/x.png');
-      places[0][0] = 'x';
-      currentPlayer = 'o';
-      player.attr('src', 'images/o.png');
-      setTimeout(checkVictory, 100);
-    } else if (places[0][0] === 0 && currentPlayer === 'o') {
-      $(this).attr('src', 'images/o.png');
-      places[0][0] = 'o';
-      currentPlayer = 'x';
-      player.attr('src', 'images/x.png');
-      setTimeout(checkVictory, 100);
-    } else {
-      alert('Invalid move!');
-    }
-  });
+  function addListeners() {
+    one.on('click', function() {
+      if (places[0][0] === 0 && currentPlayer === 'x') {
+        $(this).attr('src', 'images/x.png');
+        places[0][0] = 'x';
+        currentPlayer = 'o';
+        player.attr('src', 'images/o.png');
+        setTimeout(checkVictory, 100);
+      } else if (places[0][0] === 0 && currentPlayer === 'o') {
+        $(this).attr('src', 'images/o.png');
+        places[0][0] = 'o';
+        currentPlayer = 'x';
+        player.attr('src', 'images/x.png');
+        setTimeout(checkVictory, 100);
+      } else {
+        alert('Invalid move!');
+      }
+    });
 
-  two.on('click', function() {
-    if (places[0][1] === 0 && currentPlayer === 'x') {
-      two.attr('src', 'images/x.png');
-      places[0][1] = 'x';
-      currentPlayer = 'o';
-      player.attr('src', 'images/o.png');
-      setTimeout(checkVictory, 100);
-    } else if (places[0][1] === 0 && currentPlayer === 'o') {
-      two.attr('src', 'images/o.png');
-      places[0][1] = 'o';
-      currentPlayer = 'x';
-      player.attr('src', 'images/x.png');
-      setTimeout(checkVictory, 100);
-    } else {
-      alert('Invalid move!');
-    }
-  });
+    two.on('click', function() {
+      if (places[0][1] === 0 && currentPlayer === 'x') {
+        two.attr('src', 'images/x.png');
+        places[0][1] = 'x';
+        currentPlayer = 'o';
+        player.attr('src', 'images/o.png');
+        setTimeout(checkVictory, 100);
+      } else if (places[0][1] === 0 && currentPlayer === 'o') {
+        two.attr('src', 'images/o.png');
+        places[0][1] = 'o';
+        currentPlayer = 'x';
+        player.attr('src', 'images/x.png');
+        setTimeout(checkVictory, 100);
+      } else {
+        alert('Invalid move!');
+      }
+    });
 
-  three.on('click', function() {
-    if (places[0][2] === 0 && currentPlayer === 'x') {
-      three.attr('src', 'images/x.png');
-      places[0][2] = 'x';
-      currentPlayer = 'o';
-      player.attr('src', 'images/o.png');
-      setTimeout(checkVictory, 100);
-    } else if (places[0][2] === 0 && currentPlayer === 'o') {
-      three.attr('src', 'images/o.png');
-      places[0][2] = 'o';
-      currentPlayer = 'x';
-      player.attr('src', 'images/x.png');
-      setTimeout(checkVictory, 100);
-    } else {
-      alert('Invalid move!');
-    }
-  });
+    three.on('click', function() {
+      if (places[0][2] === 0 && currentPlayer === 'x') {
+        three.attr('src', 'images/x.png');
+        places[0][2] = 'x';
+        currentPlayer = 'o';
+        player.attr('src', 'images/o.png');
+        setTimeout(checkVictory, 100);
+      } else if (places[0][2] === 0 && currentPlayer === 'o') {
+        three.attr('src', 'images/o.png');
+        places[0][2] = 'o';
+        currentPlayer = 'x';
+        player.attr('src', 'images/x.png');
+        setTimeout(checkVictory, 100);
+      } else {
+        alert('Invalid move!');
+      }
+    });
 
-  four.on('click', function() {
-    if (places[1][0] === 0 && currentPlayer === 'x') {
-      four.attr('src', 'images/x.png');
-      places[1][0] = 'x';
-      currentPlayer = 'o';
-      player.attr('src', 'images/o.png');
-      setTimeout(checkVictory, 100);
-    } else if (places[1][0] === 0 && currentPlayer === 'o') {
-      four.attr('src', 'images/o.png');
-      places[1][0] = 'o';
-      currentPlayer = 'x';
-      player.attr('src', 'images/x.png');
-      setTimeout(checkVictory, 100);
-    } else {
-      alert('Invalid move!');
-    }
-  });
+    four.on('click', function() {
+      if (places[1][0] === 0 && currentPlayer === 'x') {
+        four.attr('src', 'images/x.png');
+        places[1][0] = 'x';
+        currentPlayer = 'o';
+        player.attr('src', 'images/o.png');
+        setTimeout(checkVictory, 100);
+      } else if (places[1][0] === 0 && currentPlayer === 'o') {
+        four.attr('src', 'images/o.png');
+        places[1][0] = 'o';
+        currentPlayer = 'x';
+        player.attr('src', 'images/x.png');
+        setTimeout(checkVictory, 100);
+      } else {
+        alert('Invalid move!');
+      }
+    });
 
-  five.on('click', function() {
-    if (places[1][1] === 0 && currentPlayer === 'x') {
-      five.attr('src', 'images/x.png');
-      places[1][1] = 'x';
-      currentPlayer = 'o';
-      player.attr('src', 'images/o.png');
-      setTimeout(checkVictory, 100);
-    } else if (places[1][1] === 0 && currentPlayer === 'o') {
-      five.attr('src', 'images/o.png');
-      places[1][1] = 'o';
-      currentPlayer = 'x';
-      player.attr('src', 'images/x.png');
-      setTimeout(checkVictory, 100);
-    } else {
-      alert('Invalid move!');
-    }
-  });
+    five.on('click', function() {
+      if (places[1][1] === 0 && currentPlayer === 'x') {
+        five.attr('src', 'images/x.png');
+        places[1][1] = 'x';
+        currentPlayer = 'o';
+        player.attr('src', 'images/o.png');
+        setTimeout(checkVictory, 100);
+      } else if (places[1][1] === 0 && currentPlayer === 'o') {
+        five.attr('src', 'images/o.png');
+        places[1][1] = 'o';
+        currentPlayer = 'x';
+        player.attr('src', 'images/x.png');
+        setTimeout(checkVictory, 100);
+      } else {
+        alert('Invalid move!');
+      }
+    });
 
-  six.on('click', function() {
-    if (places[1][2] === 0 && currentPlayer === 'x') {
-      six.attr('src', 'images/x.png');
-      places[1][2] = 'x';
-      currentPlayer = 'o';
-      player.attr('src', 'images/o.png');
-      setTimeout(checkVictory, 100);
-    } else if (places[1][2] === 0 && currentPlayer === 'o') {
-      six.attr('src', 'images/o.png');
-      places[1][2] = 'o';
-      currentPlayer = 'x';
-      player.attr('src', 'images/x.png');
-      setTimeout(checkVictory, 100);
-    } else {
-      alert('Invalid move!');
-    }
-  });
+    six.on('click', function() {
+      if (places[1][2] === 0 && currentPlayer === 'x') {
+        six.attr('src', 'images/x.png');
+        places[1][2] = 'x';
+        currentPlayer = 'o';
+        player.attr('src', 'images/o.png');
+        setTimeout(checkVictory, 100);
+      } else if (places[1][2] === 0 && currentPlayer === 'o') {
+        six.attr('src', 'images/o.png');
+        places[1][2] = 'o';
+        currentPlayer = 'x';
+        player.attr('src', 'images/x.png');
+        setTimeout(checkVictory, 100);
+      } else {
+        alert('Invalid move!');
+      }
+    });
 
-  seven.on('click', function() {
-    if (places[2][0] === 0 && currentPlayer === 'x') {
-      seven.attr('src', 'images/x.png');
-      places[2][0] = 'x';
-      currentPlayer = 'o';
-      player.attr('src', 'images/o.png');
-      setTimeout(checkVictory, 100);
-    } else if (places[2][0] === 0 && currentPlayer === 'o') {
-      seven.attr('src', 'images/o.png');
-      places[2][0] = 'o';
-      currentPlayer = 'x';
-      player.attr('src', 'images/x.png');
-      setTimeout(checkVictory, 100);
-    } else {
-      alert('Invalid move!');
-    }
-  });
+    seven.on('click', function() {
+      if (places[2][0] === 0 && currentPlayer === 'x') {
+        seven.attr('src', 'images/x.png');
+        places[2][0] = 'x';
+        currentPlayer = 'o';
+        player.attr('src', 'images/o.png');
+        setTimeout(checkVictory, 100);
+      } else if (places[2][0] === 0 && currentPlayer === 'o') {
+        seven.attr('src', 'images/o.png');
+        places[2][0] = 'o';
+        currentPlayer = 'x';
+        player.attr('src', 'images/x.png');
+        setTimeout(checkVictory, 100);
+      } else {
+        alert('Invalid move!');
+      }
+    });
 
-  eight.on('click', function() {
-    if (places[2][1] === 0 && currentPlayer === 'x') {
-      eight.attr('src', 'images/x.png');
-      places[2][1] = 'x';
-      currentPlayer = 'o';
-      player.attr('src', 'images/o.png');
-      setTimeout(checkVictory, 100);
-    } else if (places[2][1] === 0 && currentPlayer === 'o') {
-      eight.attr('src', 'images/o.png');
-      places[2][1] = 'o';
-      currentPlayer = 'x';
-      player.attr('src', 'images/x.png');
-      setTimeout(checkVictory, 100);
-    } else {
-      alert('Invalid move!');
-    }
-  });
+    eight.on('click', function() {
+      if (places[2][1] === 0 && currentPlayer === 'x') {
+        eight.attr('src', 'images/x.png');
+        places[2][1] = 'x';
+        currentPlayer = 'o';
+        player.attr('src', 'images/o.png');
+        setTimeout(checkVictory, 100);
+      } else if (places[2][1] === 0 && currentPlayer === 'o') {
+        eight.attr('src', 'images/o.png');
+        places[2][1] = 'o';
+        currentPlayer = 'x';
+        player.attr('src', 'images/x.png');
+        setTimeout(checkVictory, 100);
+      } else {
+        alert('Invalid move!');
+      }
+    });
 
-  nine.on('click', function() {
-    if (places[2][2] === 0 && currentPlayer === 'x') {
-      nine.attr('src', 'images/x.png');
-      places[2][2] = 'x';
-      currentPlayer = 'o';
-      player.attr('src', 'images/o.png');
-      setTimeout(checkVictory, 100);
-    } else if (places[2][2] === 0 && currentPlayer === 'o') {
-      nine.attr('src', 'images/o.png');
-      places[2][2] = 'o';
-      currentPlayer = 'x';
-      player.attr('src', 'images/x.png');
-      setTimeout(checkVictory, 100);
-    } else {
-      alert('Invalid move!');
-    }
-  });
+    nine.on('click', function() {
+      if (places[2][2] === 0 && currentPlayer === 'x') {
+        nine.attr('src', 'images/x.png');
+        places[2][2] = 'x';
+        currentPlayer = 'o';
+        player.attr('src', 'images/o.png');
+        setTimeout(checkVictory, 100);
+      } else if (places[2][2] === 0 && currentPlayer === 'o') {
+        nine.attr('src', 'images/o.png');
+        places[2][2] = 'o';
+        currentPlayer = 'x';
+        player.attr('src', 'images/x.png');
+        setTimeout(checkVictory, 100);
+      } else {
+        alert('Invalid move!');
+      }
+    });
+  }
+
+  addListeners();
 });
